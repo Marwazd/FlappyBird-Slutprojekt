4b53b4b9abcc75bcc201017320408ed28675f405
Marwazd <55232702+Marwazd@users.noreply.github.com>
2021-05-26 10:19:56 +0200

    import threading namespace for the timer

--- Flappy bird/Flappy bird/MainWindow.xaml.cs
+++ Flappy bird/Flappy bird/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading; //A threading namespace for the timer using System.Windows.Threading
+
 namespace Flappy_bird
 {
     /// <summary>
